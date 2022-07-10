02e2b353019d548bae468516e644f5b8906b914c
Teo Terrau <mitya.teryaew@gmail.com>
2022-07-10 15:09:46 +0400

    feat(*): allow social account linking

--- packages/next-auth/src/core/lib/callback-handler.ts
+++ packages/next-auth/src/core/lib/callback-handler.ts
@@ -189,9 +189,10 @@ export default async function callbackHandler(params: {
         // We don't want to have two accounts with the same email address, and we don't
         // want to link them in case it's not safe to do so, so instead we prompt the user
         // to sign in via email to verify their identity and then link the accounts.
-        throw new AccountNotLinkedError(
-          "Another account already exists with the same e-mail address"
-        )
+        await linkAccount(account);
+        // throw new AccountNotLinkedError(
+        //   "Another account already exists with the same e-mail address"
+        // )
       }
       // If the current user is not logged in and the profile isn't linked to any user
       // accounts (by email or provider account id)...
